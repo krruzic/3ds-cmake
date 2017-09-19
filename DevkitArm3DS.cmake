6e35c9e12d50f34233c664478ef1a94edf84a810
Thomas Farr <xtansia@xtansia.com>
2017-09-19 20:51:36 +1200

    Bump system version 47

--- DevkitArm3DS.cmake
+++ DevkitArm3DS.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_NAME 3DS-devkitARM)
-set(CMAKE_SYSTEM_VERSION 46)
+set(CMAKE_SYSTEM_VERSION 47)
 set(CMAKE_SYSTEM_PROCESSOR armv6k)
 set(3DS TRUE) # To be used for multiplatform projects
 
